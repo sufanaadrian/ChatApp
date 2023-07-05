aa27d94656c7c621db49e98e2de0e985554fe2dd
sufanaadrian <115484136+sufanaadrian@users.noreply.github.com>
2023-07-05 13:01:01 +0300

    Add files via upload

---
+++ Chat v0.1/1_login.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login_Form</title>
+    <link rel="stylesheet" href="E:\\Facultate\\ANUL 2\\SEM2\\Proiect WEB\\Chat v0.1\\style_login.css">
+</head>
+<body>
+    
+</body>
+</html>
